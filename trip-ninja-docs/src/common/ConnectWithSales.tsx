48b463cd2fae533eb346c780c684b7a80b189c37
Rob Dumont <rob.dumont@tripninja.io>
2023-04-11 15:26:42 -0400

    update text

--- trip-ninja-docs/src/common/ConnectWithSales.tsx
+++ trip-ninja-docs/src/common/ConnectWithSales.tsx
@@ -6,7 +6,7 @@ export default function ConnectWithSales() {
   return(
     <div className="row connect-banner">
       <div className="col-8">
-        <h3>Not setup with Trip Ninja?</h3>
+        <h3>Not set up with Trip Ninja?</h3>
         <p className="body-text mb-0">
           Don’t miss out on the unbeatable results from our API, talk to our sales 
           team to kickstart your Trip Ninja experience.
