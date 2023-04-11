171211d5756f0b063db557306b211725cab018ab
Rob Dumont <rob.dumont@tripninja.io>
2023-04-11 15:06:40 -0400

    Layout and style homepage

--- trip-ninja-docs/src/index.tsx
+++ trip-ninja-docs/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
 import App from "./App";
 
