d1bb6efff9fd4eee84cf821517d3a32691aa4775
Rob Dumont <rob.dumont@tripninja.io>
2023-04-12 09:20:47 -0400

    Create Pagination.tsx

---
+++ trip-ninja-docs/src/common/Pagination.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+interface PaginationProps {
+  nextPageText: string;
+  nextPageUrl: string;
+  prevPageText: string;
+  prevPageUrl: string;
+}
+
+export default function Pagination(props: PaginationProps) {
+  return(
+    <div>
+      <hr/>
+      <div className="d-flex justify-content-between">
+        <div>
+          <p>Previous Page</p>
+        </div>
+        <div>
+          <p>Next Page</p>
+        </div>
+      </div>
+    </div>
+  );
+}
