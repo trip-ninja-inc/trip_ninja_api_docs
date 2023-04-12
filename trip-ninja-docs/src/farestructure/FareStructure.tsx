9583980c39d1f4f6e949e686f6b1befdf98d8740
Rob Dumont <rob.dumont@tripninja.io>
2023-04-12 09:21:47 -0400

    styling and content updates

--- trip-ninja-docs/src/farestructure/FareStructure.tsx
+++ trip-ninja-docs/src/farestructure/FareStructure.tsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import FareStructureOverview from "./FareStructureOverview";
+import FareStructureSetup from "./FareStructureSetup";
 
 
 export default function FareStructure() {
+
+  const { view } = useParams<{ view: string }>();
+
   return(
-    <div className="my-5">
-      <h2>FareStructure</h2>
+    <div className="my-5 row">
+      <div className="col-md-3">
+
+      </div>
+      <div className="col-md-9">
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            {view === "overview" &&
+              <FareStructureOverview />
+            }
+            {view === "setup" &&
+              <FareStructureSetup />
+            }
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
