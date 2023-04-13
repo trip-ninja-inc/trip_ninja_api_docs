b87032ff520824d4c3636894306ff0e0782a0095
Rob Dumont <rob.dumont@tripninja.io>
2023-04-13 15:49:11 -0400

    Update FareStructure.tsx

--- trip-ninja-docs/src/farestructure/FareStructure.tsx
+++ trip-ninja-docs/src/farestructure/FareStructure.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import FareStructureOverview from "./FareStructureOverview";
 import FareStructureSetup from "./FareStructureSetup";
+import GetSearches from "./GetSearches";
+import GenerateSolutions from "./GenerateSolutions";
+import SideBarNav from "../common/SideBar";
 
 
 export default function FareStructure() {
@@ -9,18 +12,24 @@ export default function FareStructure() {
   const { view } = useParams<{ view: string }>();
 
   return(
-    <div className="my-5 row">
+    <div className="row">
       <div className="col-md-3">
-
+        <SideBarNav />
       </div>
       <div className="col-md-9">
-        <div className="row justify-content-center">
-          <div className="col-md-8">
+        <div className="row justify-content-center my-5">
+          <div className="col-md-9">
             {view === "overview" &&
               <FareStructureOverview />
             }
             {view === "setup" &&
               <FareStructureSetup />
+            }
+            {view === "get-searches" &&
+              <GetSearches />
+            }
+            {view === "generate-solutions" &&
+              <GenerateSolutions />
             }
           </div>
         </div>
