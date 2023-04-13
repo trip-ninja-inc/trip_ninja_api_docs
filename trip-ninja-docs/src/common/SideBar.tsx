b1629448826cb178a562a44ed43e277ffea30c2b
Rob Dumont <rob.dumont@tripninja.io>
2023-04-13 15:49:13 -0400

    Update SideBar.tsx

--- trip-ninja-docs/src/common/SideBar.tsx
+++ trip-ninja-docs/src/common/SideBar.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+interface SideBarNavProps {
+  currentView: string;
+}
 
 export default function SideBarNav() {
   return(
-    <div>
+    <div className="sidebar p-5">
+      <nav className="nav flex-column">
+        <a className="nav-link sidebar-link active" aria-current="page" href="/farestructure/overview/">Overview</a>
+        <a className="nav-link sidebar-link" href="/farestructure/setup/">Setup</a>
+        <p className="fw-semibold nav-link sidebar-link">Endpoints</p>
+        <a className="nav-link sidebar-link" href="/farestructure/get-searches/">Get Searches</a>
+        <a className="nav-link sidebar-link" href="/farestructure/generate-solutions/">Generate Solutions</a>
+      </nav>
     </div>
   );
 }
