399e3bfade89166beff4fc1abebadb715ab031df
simheather <105243595+simheather@users.noreply.github.com>
2023-04-14 11:22:07 -0300

    Update HomePage.tsx

--- trip-ninja-docs/src/home/HomePage.tsx
+++ trip-ninja-docs/src/home/HomePage.tsx
@@ -5,25 +5,6 @@ export default function HomePage() {
   return(
     <div className="my-5">
       <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-      <h2>Trip Ninja Developer Hub</h2>
-
     </div>
   );
 }
