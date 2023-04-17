51cc3d6fab071f2fe0997faaf8281fe1d7e754f0
simheather <105243595+simheather@users.noreply.github.com>
2023-04-17 09:20:50 -0300

    consistent urls

--- trip-ninja-docs/src/common/GeneralConstants.tsx
+++ trip-ninja-docs/src/common/GeneralConstants.tsx
@@ -1,7 +1,7 @@
 export enum NavLinkUrls {
     HOME = "/",
     FARESTRUCTURE = "/farestructure/overview/",
-    VI = "/virtual-interlining/overview",
-    MSDP = "/dynamic-packaging/overview",
+    VI = "/virtual-interlining/overview/",
+    MSDP = "/dynamic-packaging/overview/",
     SUPPORT = "/support-FAQ/"
 }
