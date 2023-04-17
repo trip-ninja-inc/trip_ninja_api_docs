19898d2f9caca224cea9ca3d699cbd7e83860dbf
simheather <105243595+simheather@users.noreply.github.com>
2023-04-17 10:17:25 -0300

    move footer to content pages

--- trip-ninja-docs/src/App.tsx
+++ trip-ninja-docs/src/App.tsx
@@ -25,7 +25,6 @@ export default function App() {
           </Switch>
         </Router>
       </div>
-      <Footer/>
     </body>
   );
 }
--- trip-ninja-docs/src/home/HomePage.tsx
+++ trip-ninja-docs/src/home/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "../common/Footer";
 
 
 export default function HomePage() {
   return(
     <div className="my-5">
       <h2>Trip Ninja Developer Hub</h2>
+      <Footer />
     </div>
   );
 }
